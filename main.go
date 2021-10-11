05e2f8f3016f2382d239060de7c592ccedd8b47e
jbrard <Joshuaeli1234@gmail.com>
2021-10-10 22:59:25 -0400

    Seperated auth from package main

--- main.go
+++ main.go
@@ -1,184 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"crypto/rand"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
-	"os"
-	"os/exec"
-	"runtime"
+	"malctl/auth"
+	"malctl/client"
 
 	"github.com/nstratos/go-myanimelist/mal"
-	"golang.org/x/oauth2"
-	"malctl/client"
-)
-
-const (
-	cacheName     = "secrets/auth-example-token-cache.txt"
-	clientSecrets = "secrets/client.txt"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-}
-
-type ClientInfo struct {
-	ClientID     string `json:"clientID"`
-	ClientSecret string `json:"clientSecret"`
-}
-
-func run() error {
-
-	clientFile, err := os.ReadFile(clientSecrets)
+	tokenClient, err := auth.CreateTokenClient()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var clientInfo ClientInfo
-
-	err = json.Unmarshal(clientFile, &clientInfo)
-	ctx := context.Background()
-	state := ""
-	tokenClient, err := authenticate(ctx, clientInfo.ClientID, clientInfo.ClientSecret, state)
-	if err != nil {
-		return err
-	}
-
 	c := client.DemoClient{
 		Client: mal.NewClient(tokenClient),
 	}
-
-	return c.Showcase(ctx)
-}
-
-func authenticate(ctx context.Context, clientID, clientSecret, state string) (*http.Client, error) {
-	// Prepare the oauth2 configuration with your application ID, secret, the
-	// MyAnimeList authentication and token URLs as specified in:
-	//
-	// https://myanimelist.net/apiconfig/references/authorization
-	conf := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:   "https://myanimelist.net/v1/oauth2/authorize",
-			TokenURL:  "https://myanimelist.net/v1/oauth2/token",
-			AuthStyle: oauth2.AuthStyleInParams,
-		},
-	}
-
-	oauth2Token, err := loadCachedToken()
-	if err == nil {
-		refreshedToken, err := conf.TokenSource(ctx, oauth2Token).Token()
-		if err == nil && (oauth2Token != refreshedToken) {
-			fmt.Println("Caching refreshed oauth2 token...")
-			if err := cacheToken(*refreshedToken); err != nil {
-				return nil, fmt.Errorf("caching refreshed oauth2 token: %s", err)
-			}
-			return conf.Client(ctx, refreshedToken), nil
-		}
-		return conf.Client(ctx, oauth2Token), nil
-	}
-
-	// Generate a code verifier, a high-entropy cryptographic random string. It
-	// will be set as the code_challenge in the authentication URL. It should
-	// have a minimum length of 43 characters and a maximum length of 128
-	// characters.
-	const codeVerifierLength = 128
-	codeVerifier, err := generateCodeVerifier(codeVerifierLength)
+	ctx := context.Background()
+	err = c.Showcase(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("generating code verifier: %v", err)
-	}
-
-	// Produce the authentication URL where the user needs to be redirected and
-	// allow your application to access their MyAnimeList data.
-	authURL := conf.AuthCodeURL(state,
-		oauth2.SetAuthURLParam("code_challenge", codeVerifier),
-	)
-
-	err = openBrowser(authURL)
-	if err != nil {
-		fmt.Println("Could not open browser.")
-	}
-
-	fmt.Printf("Your browser should open: %v\n", authURL)
-	fmt.Print("After authenticating, copy the code from the browser URL and paste it here: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	code := scanner.Text()
-	if scanner.Err() != nil {
-		return nil, fmt.Errorf("reading code from terminal: %v", err)
-	}
-
-	// Exchange the authentication code for a token. MyAnimeList currently only
-	// supports the plain code_challenge_method so to verify the string, just
-	// make sure it is the same as the one you entered in the code_challenge.
-	token, err := conf.Exchange(ctx, code,
-		oauth2.SetAuthURLParam("code_verifier", codeVerifier),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("exchanging code for token: %v", err)
-	}
-	fmt.Println("Authentication was successful. Caching oauth2 token...")
-	if err := cacheToken(*token); err != nil {
-		return nil, fmt.Errorf("caching oauth2 token: %s", err)
-	}
-
-	return conf.Client(ctx, token), nil
-}
-
-func cacheToken(token oauth2.Token) error {
-	b, err := json.MarshalIndent(token, "", "   ")
-	if err != nil {
-		return fmt.Errorf("marshaling token %s: %v", token, err)
-	}
-	err = os.WriteFile(cacheName, b, 0644)
-	if err != nil {
-		return fmt.Errorf("writing token %s to file %q: %v", token, cacheName, err)
-	}
-	return nil
-}
-
-func loadCachedToken() (*oauth2.Token, error) {
-	b, err := os.ReadFile(cacheName)
-	if err != nil {
-		return nil, fmt.Errorf("reading oauth2 token from cache file %q: %v", cacheName, err)
-	}
-	token := new(oauth2.Token)
-	if err := json.Unmarshal(b, token); err != nil {
-		return nil, fmt.Errorf("unmarshaling oauth2 token: %v", err)
-	}
-	return token, nil
-}
-
-func generateCodeVerifier(length int) (string, error) {
-	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstvuwxyz" +
-		"0123456789-._~"
-	bytes := make([]byte, length)
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
-	}
-	for i, b := range bytes {
-		bytes[i] = charset[b%byte(len(charset))]
-	}
-	return string(bytes), nil
-}
-
-func openBrowser(url string) error {
-	switch runtime.GOOS {
-	case "linux":
-		return exec.Command("xdg-open", url).Start()
-	case "windows":
-		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		return exec.Command("open", url).Start()
-	default:
-		return fmt.Errorf("openBrowser: unsupported operating system: %v", runtime.GOOS)
+		log.Fatal(err)
 	}
 }
